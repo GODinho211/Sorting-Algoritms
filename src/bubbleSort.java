6495120593a48b2ac8c79364ec60ee3c21f2bac8
Jose Costa <1222129@isep.ipp.pt>
2025-11-10 18:00:33 +0000

    selection Sort

--- src/bubbleSort.java
+++ src/bubbleSort.java
@@ -9,17 +9,18 @@ void main() {
     //Bubble Sort
     for(int i =0; i<arr.length-1; i++){
         for(int j=0; j<arr.length-i-1; j++){
+            boolean flag = true;
             if(arr[j]>arr[j+1]){
                 int temp = arr[j];
                 arr[j] = arr[j+1];
                 arr[j+1] = temp;
-            }
 
+            }
             System.out.println(Arrays.toString(arr));
         }
 
     }
-    System.out.println("Array original: " + Arrays.toString(arr));
+    System.out.println("Array Ordenado: " + Arrays.toString(arr));
     System.out.println(arr.length);
 
 
--- src/selectionSort.java
+++ src/selectionSort.java
@@ -1,7 +1,33 @@
 void main(){
-    
+    int[] arr = {5, 2, 9, 1, 5, 6};
+    System.out.println("Array original: " + Arrays.toString(arr));
+    int temp=0;
+    int index=0;
+
+    for(int i =0; i < arr.length-1; i++){
+        index=i;
+        for(int j = i+1; j < arr.length; j++) {
+            if (arr[index] > arr[j]) {
+                index = j;
+            }
+        }
+            temp =arr[index];
+            arr[index]=arr[i];
+            arr[i]=temp;
 
 
-
+        System.out.println(arr[i]);
+    }
+    System.out.println("Array Ordenado: " + Arrays.toString(arr));
 
 }
+
+
+
+
+
+
+
+
+
+
